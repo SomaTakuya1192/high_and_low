2fb3aea3409a1cfdae33f9845fb53093d7d597e2
相馬　拓也 <soumatakuya@admin-no-MacBook-Air-2.local>
2019-03-25 23:42:58 +0900

    high and low ゲーム

---
+++ header.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <link href="style.css" rel="stylesheet" type="text/css">
+  <title>High and Low</title>
+</head>
+<body id="top">
+  <h2>High & Lowゲーム</h2>
+  <div class="bottm_line"></div>
---
+++ index.php
@@ -0,0 +1,43 @@
+<?php include ('header.php'); ?>
+
+<?php
+$min = 0;
+$max = 13;
+$rand = rand($min, $max);
+//$cardsに1~13を入れる
+/*
+$cards = "";
+for($i = 1; $i <= 13; $i++){
+  //echo $i.".png,";
+$cards .= '"';
+$cards .= sprintf("%02d",$i);
+$cards .= '.png';
+$cards .= '", ';
+}
+//$total_cardsにトランプ0~13.pngを入れる。
+$total_cards = '"Jk.png", '.$cards;
+rtrim($total_cards, ', ');
+//var_dump($array);
+echo $total_cards;
+echo "<br />";
+$card = array($total_cards);
+echo $card[1];
+*/
+
+//妥協策
+$cards = array("Jk.png", "01.png", "02.png", "03.png", "04.png", "05.png", "06.png", "07.png", "08.png", "09.png", "10.png", "11.png", "12.png", "13.png");
+?>
+
+<form action="process.php" method="post">
+<!--画像出力-->
+<img class="position_reft" src=cards/<?echo $cards[$rand];?>>
+<img class="position_light" src=cards/bg.png>
+<br>
+<!--ラジオボタン-->
+<div class="high"><input type="radio" name="select" value="High" required="required">High</div>
+<div class="low" ><input type="radio" name="select" value="Low" required="required">Low</div>
+<input class="hidden" type="hidden" name="leftCard" value="<?echo $rand; ?>">
+<br>
+<input type="submit" value="決定">
+</form>
+<?php include ('footer.php'); ?>
