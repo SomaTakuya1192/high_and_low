f09ac51277d5f938cdba8d6dc9bb945040de6d83
相馬　拓也 <soumatakuya@admin-no-MacBook-Air-2.local>
2019-03-27 11:00:55 +0900

    インデント整える

--- index.php
+++ index.php
@@ -9,15 +9,20 @@ $cards = array("00.png", "01.png", "02.png", "03.png", "04.png", "05.png", "06.p
 
 <form action="process.php" method="post">
 <!--画像出力-->
-<img class="position_reft" src=cards/<?echo $cards[$rand];?>>
-<img class="position_light" src=cards/bg.png>
-<br>
+  <img class="position_reft" src="cards/<?echo $cards[$rand];?>">
+  <img class="position_light" src="cards/bg.png">
+    <br>
 <!--ラジオボタン-->
-<div class="high"><input type="radio" name="select" value="High" required="required">High</div>
-<div class="low" ><input type="radio" name="select" value="Low" required="required">Low</div>
-<input class="hidden" type="hidden" name="leftCard" value="<?echo $rand; ?>">
-<br>
-<input type="submit" value="決定">
+  <div class="high">
+    <input type="radio" name="select" value="High" required="required">
+    <span>High</span>
+  </div>
+  <div class="low" ><input type="radio" name="select" value="Low" required="required">
+    <span>Low</span>
+  </div>
+  <input class="hidden" type="hidden" name="leftCard" value="<?echo $rand; ?>">
+    <br>
+  <input type="submit" value="決定">
 </form>
 
 <?php include ('footer.php'); ?>
